5e84ffe2cc11b29e51a47465d10a9075c2c92d6d
FalakChhikara <falakchhikara2001@gmail.com>
2021-01-14 00:54:38 +0530

    hello world done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+
+//(what to show in JSX format(single object,
+                //  or inside single div 
+                // or array format[obj1, obj2, ...]
+    // most imp // <React.Fragment> </React.Fragment>)
+    //          // <>  </>  or simply do this
+// , where to show,
+//  callback)
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    {/* {for js expression} */}
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +21,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
